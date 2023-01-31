a0764c3c44e2c7a41a5ed8f2045756e63c2c8283
mostaard <jonas@mostaard.be>
2023-01-31 16:17:13 +0100

    Feat: Add scraper for OVP pages.

--- scrapers/ovp_scraper.py
+++ scrapers/ovp_scraper.py
@@ -1,3 +1,5 @@
+import csv
+import csv
 import logging
 from pathlib import Path
 from typing import TextIO
@@ -7,13 +9,36 @@ from bs4 import BeautifulSoup
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+header = ['course', 'section', 'section_title', 'ex_type', 'module', 'filename',
+          'ex_name']
 
-def scrape_ovp(file: TextIO):
+data = []
+modules = []
+
+
+def scrape_ovp(file: TextIO, group, section):
     soup = BeautifulSoup(file.read(), features="html.parser")
+    title = soup.find('title')
+    section_title = title.contents[0] if title and title.contents else ''
     for a in soup.find_all('a'):
+        row = [group, section, section_title]
         try:
-            log.info(f"{a.attrs['href']} {a.contents[0]}")
-        except (KeyError, IndexError):
+            if a.attrs['href'].startswith("../..") and not a.attrs['href'].endswith(
+                    'jpg'):
+                link = a.attrs['href'].replace("\\", '/').split("/")
+                module = link[4]
+                if [module] not in modules:
+                    modules.append([module])
+                # ex_type
+                row.append(link[3])
+                row.append(module)
+                # filename
+                row.append(link[5].split('?')[0])
+                # ex_name
+                row.append(
+                    a.contents[0].replace('\t', '').replace('\n', '').replace(',', ''))
+                data.append(row)
+        except (KeyError, IndexError, TypeError):
             log.error('something missing')
 
 
@@ -22,11 +47,25 @@ decoding_error = 0
 for path in path_list:
     group_info = path.name.split('_')
     if path.suffix == '.htm' and group_info[0] == 'CLT' and len(group_info) >= 4:
-        log.debug(group_info[1])
-        log.debug(group_info[3].replace('.htm', ''))
+        # course
+        group = group_info[1]
+        # section
+        section = group_info[3].replace('.htm', '')
         try:
             with open(path, encoding='utf8') as fh:
-                scrape_ovp(fh)
+                scrape_ovp(fh, group, section)
         except UnicodeDecodeError:
             with open(path, encoding='cp1252') as fh:
-                scrape_ovp(fh)
+                scrape_ovp(fh, group, section)
+
+with open('output/ovp.csv', 'w', encoding='UTF8', newline='') as out:
+    writer = csv.writer(out)
+    writer.writerow(header)
+    for r in data:
+        writer.writerow(r)
+
+with open('output/modules.csv', 'w', encoding='UTF8', newline='') as out:
+    writer = csv.writer(out)
+    writer.writerow(['module', 'language'])
+    for r in modules:
+        writer.writerow(r)
